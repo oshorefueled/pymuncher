c385fbf0b4efedd8b5acecf28c1675d7a08263cd
Osho Klinsmann <oshoklinsmann@gmail.com>
2017-02-21 04:20:33 +0100

    unused imports removed

--- src/app.py
+++ src/app.py
@@ -1,11 +1,7 @@
-import requests
-from bs4 import BeautifulSoup
 from muncher import Muncher
+munch = Muncher("http://www.konga.com/canvas-painting-dancing-ladies-2779922", "span")
+content = munch.getContent()
 
-
-muncher = Muncher("http://www.konga.com/canvas-painting-dancing-ladies-2779922", "span")
-content = muncher.getContent()
-
-print("price of the product is %s" % (muncher.find(content, "span", "price")))
+print("price of the product is %s" % (munch.find(content, "span", "price")))
 
 
